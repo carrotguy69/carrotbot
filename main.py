f34cddac2ef7c12ea04194b6299c5c472a1b623e
Austin <72773619+carrotguy69@users.noreply.github.com>
2021-04-11 15:31:37 -0700

    added a status and stuff and a status and stuff

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import discord, asyncio, os
 from discord.ext import commands
+from discord import Activity, ActivityType
 
 client = commands.Bot(command_prefix=";", intents = discord.Intents.all())
-version = "3.0"
+client.remove_command('help')
 
 @client.event
 async def on_ready():
-    print(f"* Running carrotbot v.{version}")
-    client.remove_command('help')
-
-extensions = ["cogs.commands.fun.echo", "cogs.commands.moderation.mute", "cogs.events.welcome"]
+    print(f"* Online")
+    await client.change_presence(status =  discord.Status.online, activity = Activity(type = ActivityType.watching, name = "Type ;help"))
+    
+extensions = [
+            "cogs.commands.fun.echo", 
+            "cogs.commands.moderation.mute", 
+            "cogs.events.welcome", 
+            "cogs.commands.moderation.nick", 
+            "cogs.commands.info.git",
+            "cogs.commands.info.helpcmd",
+            "cogs.events.errors"
+            ]
 
 if __name__ == '__main__':
     for ext in extensions:
@@ -18,10 +27,7 @@ if __name__ == '__main__':
 
 
 def read_token():
-    scriptpath = os.path.dirname(__file__)
-    filename = os.path.join(scriptpath, "token.txt")
-    tfile = open(filename)
-    token = tfile.readlines()
+    token = open(os.path.join(os.path.dirname(__file__), "token.txt")).readlines()
     return token[0].strip()
 
 token = read_token()
