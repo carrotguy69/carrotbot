ed98421ef5dcffa90ee43754a8ddbb19314892a5
Austin <72773619+carrotguy69@users.noreply.github.com>
2021-04-12 21:35:39 -0700

    Update onmsg.py

--- cogs/events/onmsg.py
+++ cogs/events/onmsg.py
@@ -5,6 +5,9 @@ class onmsg(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    hellochat = ["https://tenor.com/view/hello-chat-gif-21073786", "https://tenor.com/view/youtube-twitch-meme-chair-office-gif-19928140", "https://tenor.com/view/hello-chat-big-chungus-meme-roblox-fat-gif-20170607", "https://tenor.com/view/hello-chat-hello-marvel-thanos-portal-gif-20110933", "https://tenor.com/view/hello-chat-phantom-forces-roblox-gif-20662777", "https://tenor.com/view/hello-chat-hello-agent-jonsey-jonsey-fortnite-gif-20110941", "https://tenor.com/view/hello-chat-kin-planet-gif-20744793", "https://tenor.com/view/hi-chat-gif-20883935"]
+
+        
     @commands.guild_only()
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -17,9 +20,8 @@ class onmsg(commands.Cog):
         #says hi back
         if message.content.startswith("hi" or "hello"):
             await asyncio.sleep(1)
-            hellochat = ["https://tenor.com/view/hello-chat-gif-21073786", "https://tenor.com/view/youtube-twitch-meme-chair-office-gif-19928140", "https://tenor.com/view/hello-chat-big-chungus-meme-roblox-fat-gif-20170607", "https://tenor.com/view/hello-chat-hello-marvel-thanos-portal-gif-20110933", "https://tenor.com/view/hello-chat-phantom-forces-roblox-gif-20662777", "https://tenor.com/view/hello-chat-hello-agent-jonsey-jonsey-fortnite-gif-20110941", "https://tenor.com/view/hello-chat-kin-planet-gif-20744793", "https://tenor.com/view/hi-chat-gif-20883935"]
             await message.channel.send(random.choice(hellochat))
-        
+       
         else:
             return
 
