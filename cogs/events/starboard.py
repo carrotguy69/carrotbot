fefe3e6cc1a2205b1af1cadbde312973564c7201
Austin <72773619+carrotguy69@users.noreply.github.com>
2021-04-18 12:38:49 -0700

    in theory this should work BUT IT DONT

--- cogs/events/starboard.py
+++ cogs/events/starboard.py
@@ -1,15 +1,35 @@
-import discord
+import discord, resources
 from discord.ext import commands
 
-class starboard(commands.Cog):
-  def __init__(self, client):
-    self.client = client
+class stars(commands.Cog):
+	def __init__(self, client):
+		self.client = client
     
-    @commands.Cog.listener()
-    on_raw_reaction_add(self, payload):
-      if payload.emoji.name == "⭐":
-        msg = payload.message
-        
-        embed = discord.Embed(title = "Starred Message", description = f"{[msg.content]()}")
-        
-        # fuck didnt finish
+	@commands.Cog.listener()
+	async def on_raw_reaction_add(self, payload):
+		print("a")
+		starboard = resources.channels.grab_channel("Starboard")
+
+		if payload.emoji.name == "⭐":
+			msg = await self.client.get_channel(payload.channel.id).fetch_message(payload.message_id)
+
+			if msg.content:
+				embed = discord.Embed(description = f"{msg.content}\n[Jump!](msg.jump_url)", colour = discord.Colour(0xff8b00))
+				embed.set_author(name = msg.author.name, icon_url = msg.author.avatar_url)
+				embed.set_footer(f"🌟 New starred message ⭐")
+
+				if len(msg.attatchments):
+					embed.set_image(url = msg.attatchments[0].url)
+
+	
+			else:
+				embed = discord.Embed(colour = discord.Colour(0xff8b00))
+				embed.set_image(url = msg.attatchments[0].url)
+				embed.set_footer(f"⭐ New starred message 🌟")
+
+				await starboard.send(embed = embed)
+			
+			await starboard.send(embed = embed)
+
+def setup(client):
+	client.add_cog(stars(client))
