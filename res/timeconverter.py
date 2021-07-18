9a4d13e933cbab33983b4f38328d11467bae314e
Austin <72773619+carrotguy69@users.noreply.github.com>
2021-07-18 00:22:08 -0700

    Delete timeconverter.py

--- res/timeconverter.py
+++
@@ -1,8 +0,0 @@
-time_convert = {"s": 1, "m": 60, "h": 3600, "d": 86400}
-
-def convert(time):
-    try:
-        return int(time[:-1]) * time_convert[time[-1]]
-    
-    except:
-        return time
